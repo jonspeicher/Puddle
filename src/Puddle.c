4fdbede11af5dc5837df0dd7598602a00dc3b5bc
Jon Speicher <jon.speicher@gmail.com>
2013-07-31 23:22:16 -0400

    Add rudimentary debug layer

--- src/Puddle.c
+++ src/Puddle.c
@@ -11,12 +11,17 @@ PBL_APP_INFO(MY_UUID,
              APP_INFO_STANDARD_APP);
 
 Window window;
-
+TextLayer debug_layer;
 
 void handle_init(AppContextRef ctx) {
-
   window_init(&window, "Puddle Main");
   window_stack_push(&window, true /* Animated */);
+
+  text_layer_init(&debug_layer, GRect(0, 65, 144, 30));
+  text_layer_set_text_alignment(&debug_layer, GTextAlignmentCenter);
+  text_layer_set_text(&debug_layer, "Debug Layer");
+  text_layer_set_font(&debug_layer, fonts_get_system_font(FONT_KEY_ROBOTO_CONDENSED_21));
+  layer_add_child(&window.layer, &debug_layer.layer);
 }
 
 
