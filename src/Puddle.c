059e23be328e8df2bfe03254b38eafb12ca31210
Jon Speicher <jon.speicher@gmail.com>
2013-08-01 02:23:04 -0400

    Add first stab at http request and callbacks

--- src/Puddle.c
+++ src/Puddle.c
@@ -11,15 +11,62 @@ PBL_APP_INFO(MY_UUID,
              DEFAULT_MENU_ICON,
              APP_INFO_STANDARD_APP);
 
+#define DUMMY_COOKIE 100
+
 Window window;
 TextLayer debugLayer;
 
 void select_single_click_handler(ClickRecognizerRef recognizer, Window *window) {
-  text_layer_set_text(&debugLayer, "Select!");
+  DictionaryIterator *body;
+  HTTPResult result = http_out_get("http://latrice:9000/test.json", DUMMY_COOKIE, &body);
+
+  switch ((int) result)
+  {
+    case HTTP_OK:
+      break;
+    case HTTP_BUSY:
+      text_layer_set_text(&debugLayer, "busy");
+      return;
+    case HTTP_INVALID_ARGS:
+      text_layer_set_text(&debugLayer, "invalid args");
+      return;
+    case HTTP_NOT_ENOUGH_STORAGE:
+      text_layer_set_text(&debugLayer, "storage");
+      return;
+    case HTTP_INTERNAL_INCONSISTENCY:
+      text_layer_set_text(&debugLayer, "incons");
+      return;
+    default:
+      text_layer_set_text(&debugLayer, "default");
+      return;
+  }
+
+  dict_write_int32(body, 1, 100);
+  dict_write_int32(body, 2, 200);
+  dict_write_cstring(body, 3, "us");
+
+  if (http_out_send() != HTTP_OK) {
+    text_layer_set_text(&debugLayer, "send Failed!");
+    return;
+  }
+}
+
+void up_single_click_handler(ClickRecognizerRef recognizer, Window *window) {
+  text_layer_set_text(&debugLayer, "Reset!");
+  app_message_out_release();
 }
 
 void click_config_provider(ClickConfig **config, Window *window) {
   config[BUTTON_ID_SELECT]->click.handler = (ClickHandler) select_single_click_handler;
+  config[BUTTON_ID_UP]->click.handler = (ClickHandler) up_single_click_handler;
+}
+
+void http_request_failure_handler(int32_t cookie, int http_status, void* context) {
+  text_layer_set_text(&debugLayer, "request Failed!");
+}
+
+void http_request_success_handler(int32_t cookie, int http_status, DictionaryIterator* received, void* context) {
+    text_layer_set_text(&debugLayer, "request Success!");
 }
 
 void handle_init(AppContextRef ctx) {
@@ -33,6 +80,14 @@ void handle_init(AppContextRef ctx) {
   layer_add_child(&window.layer, &debugLayer.layer);
 
   window_set_click_config_provider(&window, (ClickConfigProvider) click_config_provider);
+
+  http_set_app_id(54332);
+  http_register_callbacks((HTTPCallbacks) {
+    .failure = http_request_failure_handler,
+    .success = http_request_success_handler
+    },
+    (void*) ctx
+  );
 }
 
 
