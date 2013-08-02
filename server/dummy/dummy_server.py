7558f0ed7c14cd4a1cfb87fdefc631adf8d1aff0
Jon Speicher <jon.speicher@gmail.com>
2013-08-02 17:38:37 -0400

    Clean up post formatting and response code

--- server/dummy/dummy_server.py
+++ server/dummy/dummy_server.py
@@ -10,16 +10,17 @@ SERVER_PORT = 9000
 
 class JsonPostResponder(RequestHandlerClass):
     def do_POST(self):
-        print '---> dummy server: got post!'
+        print '\n---> dummy server: got post!'
         print 'command:', self.command
         print 'path:', self.path
         print 'headers:\n\n', self.headers
 
+        content_length = int(self.headers['Content-Length'])
+        content = self.rfile.read(content_length)
+        print 'content:\n\n', content, '\n'
+
         self.send_response(200)
         self.end_headers()
-        varLen = int(self.headers['Content-Length'])
-        postVars = self.rfile.read(varLen)
-        print postVars
 
 server_address = (SERVER_NAME, SERVER_PORT)
 httpd = BaseHTTPServer.HTTPServer(server_address, JsonPostResponder)
