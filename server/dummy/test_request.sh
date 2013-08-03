f138a44bf7bc362b69e1442bb2d4e7e638cec890
Jon Speicher <jon.speicher@gmail.com>
2013-08-03 01:52:12 -0400

    Change test request to specify a filename in path

--- server/dummy/test_request.sh
+++ server/dummy/test_request.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST -d @request.json http://latrice.local:9000 --header "Content-Type:application/json"
+curl -X POST -d @request.json http://latrice.local:9000/response.json --header "Content-Type:application/json"
