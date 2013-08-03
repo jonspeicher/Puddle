aaa696b01de406d9135035017ff353c022084324
Jon Speicher <jon.speicher@gmail.com>
2013-08-03 16:46:48 -0400

    Remove hard-coded host name from dummy server test script

--- server/dummy/test_request.sh
+++ server/dummy/test_request.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST -d @request.json http://latrice.local:9000/response.json --header "Content-Type:application/json"
+curl -X POST -d @request.json http://`hostname`:9000/response.json --header "Content-Type:application/json"
