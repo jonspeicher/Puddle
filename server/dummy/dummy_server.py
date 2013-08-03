fb4aa211f64ed6fdc0443d03dd02dc52fc882978
Jon Speicher <jon.speicher@gmail.com>
2013-08-03 02:18:32 -0400

    Clean up and refactor printing of request

--- server/dummy/dummy_server.py
+++ server/dummy/dummy_server.py
@@ -12,12 +12,16 @@ class JsonPostResponder(RequestHandlerClass):
     def _get_content_from_stream(self, length, stream):
         return stream.read(length)
 
+    def _transaction_string(self, command, path, headers, content):
+        return '%s %s\n%s%s\n' % (command, path, headers, content)
+
+    def _print_request(self, *request):
+        print('--> %s' % self._transaction_string(*request))
+
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         content = self._get_content_from_stream(content_length, self.rfile)
-
-        print('\n--- %s%s\n%s' % (self.command, self.path, self.headers))
-        print content, '\n'
+        self._print_request(self.command, self.path, self.headers, content)
 
         self.send_response(200)
         self.end_headers()
